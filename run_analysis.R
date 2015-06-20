f113399103a0691a15b39adfcf6576d1f0a72015
elegantcoderM <manudhundi@yahoo.com>
2015-06-20 16:06:37 -0700

    Merge the data sets to form one data set

---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+###############################################################
+# Merge the training and the test sets to create one data set #
+###############################################################
+data_set_type <- c("train", "test")
+data_file_name_type <- c("subject", "X", "y")
+
+# Read all files
+# the below nested for loops create 6 variables:
+# X_train, y_train, subject_train, X_test, y_test, subject_test
+for (type in data_set_type) {
+  for (name in data_file_name_type) {
+    assign(paste(name, type, sep = "_"),
+           read.table(paste("./", type, "/", name, "_", type, ".txt",
+                            sep = "")))
+  }
+}
+
+# Merge the read training and test data
+# the merged data is put into variables: X_all, y_all, subject_all
+for (name in data_file_name_type) {
+  assign(paste(name, "_all", sep = ""),
+         rbind(get(paste(name, data_set_type[1], sep = "_")),
+               get(paste(name, data_set_type[2], sep = "_"))))
+}
+
+# Add feature names
+features <- read.table("features.txt")
+colnames(X_all) <- as.character(features[,2])
+
+colnames(y_all) <- "activity"
+colnames(subject_all) <- "person_id"
+
+# Merge subject, activity and observed value into one data variable
+data <- cbind(subject_all, y_all, X_all)
+
+
+########################################################################
+# Extract only the measurements on the mean and standard deviation for #
+# each measurement.                                                    #
+########################################################################
+
+
+
+# Use descriptive activity names to name the activities in the data set
+
+
+# Appropriately label the data set with descriptive variable names.
+
+# From the data set in step 4, create a second, independent tidy data
+# set with the average of each variable for each activity and each subject
